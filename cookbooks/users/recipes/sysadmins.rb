a56aecc5a9061ec899b935b29555d5379ad09026
Wes Morgan <cap10morgan@gmail.com>
2011-11-04 11:39:50 -0600

    add sudoers setup to users cookbook

--- cookbooks/users/recipes/sysadmins.rb
+++ cookbooks/users/recipes/sysadmins.rb
@@ -7,9 +7,9 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -66,4 +66,10 @@ end
 group "sysadmin" do
   gid 2300
   members sysadmin_group
+end
+
+template "/etc/sudoers.d/sysadmin_group" do
+  source "sudoers.erb"
+  mode "0440"
+  variables :sudo_groups => { :name => 'sysadmin', :sudo_perms => 'ALL=(ALL:ALL) ALL' }
 end
