5e47d2aa17c3b5096047ea239eed9f3cbdf36721
Wes Morgan <cap10morgan@gmail.com>
2011-11-04 11:03:54 -0600

    delete old cookbook stuff

--- site-cookbooks/dnsmadeeasy/metadata.rb
+++
@@ -1,6 +0,0 @@
-maintainer       "Third Sector Design"
-maintainer_email "support@thirdsectordesign.org"
-license          "Apache 2.0"
-description      "Updates dynamic DNS records at dnsmadeeasy.com"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.1"
