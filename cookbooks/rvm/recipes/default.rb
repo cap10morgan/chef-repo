777c4ef9de2101e1822b0e16b26cfcebe8301cb6
Wes Morgan <cap10morgan@gmail.com>
2011-12-23 14:10:23 -0500

    fix rvm installation test

--- cookbooks/rvm/recipes/default.rb
+++ cookbooks/rvm/recipes/default.rb
@@ -17,7 +17,7 @@ end
 bash "installing system-wide RVM stable" do
   user "root"
   code "bash -s stable < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer )"
-  not_if "which rvm"
+  not_if %q{ [[ `type rvm | head -n 1` == "rvm is a function" ]] }
 end
 
 bash "upgrading to RVM head" do
