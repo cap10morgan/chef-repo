7d33ae8ba368b39aa72c79398f4dc9f034353bc9
Wes Morgan <cap10morgan@gmail.com>
2011-12-23 10:16:26 -0500

    delete bug genie cookbook

--- cookbooks/bug_genie/attributes/default.rb
+++
@@ -1,6 +0,0 @@
-default['bug_genie']['version']  = "3.1.4"
-default['bug_genie']['checksum'] = "6a4dfdb110477131ef27edce39306671a674095f39be39fcd6246e6929460c00"
-default['bug_genie']['dir'] = "/var/www/bug_genie"
-default['bug_genie']['db']['database'] = 'bug_genie'
-default['bug_genie']['db']['user'] = 'bug_genie'
-default['bug_genie']['db']['table_prefix'] = 'tbg3_'
