8693eb3c2657d01453ff86f98e09591f95c2d1de
Wes Morgan <cap10morgan@gmail.com>
2012-01-20 13:34:09 -0700

    get the tvblog::wp-theme recipe working

--- cookbooks/tvblog/recipes/wp-theme.rb
+++ cookbooks/tvblog/recipes/wp-theme.rb
@@ -6,21 +6,37 @@ else
   branch_name = "master"
 end
 
+tvblog_dir = "#{node[:turbovote][:app_root]}/tvblog"
+
+directory "#{node[:wordpress][:dir]}/wp-content/themes/turbovote" do
+  owner "www-data"
+  group "www-data"
+  mode 0755
+end
+
 template "#{node[:turbovote][:app_root]}/.ssh/git-ssh-wrapper" do
   source "git-ssh-wrapper.erb"
   owner node[:turbovote][:user]
   group node[:turbovote][:user]
   mode 0700
   variables :private_key_path => "#{node[:turbovote][:app_root]}/.ssh/id_dsa"
-  notifies :sync, "git[wp-theme]", :immediately
 end
 
 git "wp-theme" do
   repository node[:tvblog][:git_repo]
-  destination "#{node[:wordpress][:dir]}/wp-content/themes/turbovote"
+  destination tvblog_dir
   depth 1
   ssh_wrapper "#{node[:turbovote][:app_root]}/.ssh/git-ssh-wrapper"
   revision branch_name
-  action :nothing # the git-ssh-wrapper template resource will trigger this
-  user "deploy"
+end
+
+execute "copy-wp-theme" do
+  not_if "diff -qr #{tvblog_dir}/themes/turbovote #{node[:wordpress][:dir]}/wp-content/themes/turbovote"
+  commands = [
+    "cp -a #{tvblog_dir}/themes/turbovote #{tvblog_dir}/turbovote-wp-theme.copy",
+    "rm -rf /tmp/turbovote-wp-theme.old",
+    "mv #{node[:wordpress][:dir]}/wp-content/themes/turbovote /tmp/turbovote-wp-theme.old",
+    "mv #{tvblog_dir}/turbovote-wp-theme.copy #{node[:wordpress][:dir]}/wp-content/themes/turbovote",
+  ]
+  command commands.join(' && ')
 end
