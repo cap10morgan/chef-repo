b704ea7269b42c2438aa4f0ddfb0bccaeb51f0a8
Wes Morgan <cap10morgan@gmail.com>
2012-01-20 10:19:11 -0700

    fix bug in turbovote database recipe

--- cookbooks/turbovote/recipes/database.rb
+++ cookbooks/turbovote/recipes/database.rb
@@ -1,7 +1,8 @@
 mysql_creds = Chef::EncryptedDataBagItem.load("passwords", "mysql")
 
+password_option = "#{node['mysql']['server_root_password'].empty? ? '' : '-p' }#{node['mysql']['server_root_password']}"
+
 execute "create-database" do
-  password_option = "#{node['mysql']['server_root_password'].empty? ? '' : '-p' }#{node['mysql']['server_root_password']}"
   command "/usr/bin/mysqladmin -u root #{password_option} create #{node['turbovote']['database_name']}"
   not_if "/usr/bin/mysql -u root #{password_option} -e 'show databases;' | grep -q '^#{node['turbovote']['database_name']}$'"
 end
@@ -23,9 +24,13 @@ template "/var/cache/local/mysql/grant-query.sql" do
 end
 
 execute "mysql-set-privileges" do
-  password_option = "#{node['mysql']['server_root_password'].empty? ? '' : '-p' }#{node['mysql']['server_root_password']}"
   query_file = "/var/cache/local/mysql/grant-query.sql"
   command "/usr/bin/mysql -u root #{password_option} < #{query_file} && rm #{query_file}"
+  action :nothing
+  notifies :run, "execute[mysql-flush-privileges]", :immediately
+end
+
+execute "mysql-flush-privileges" do
   command "/usr/bin/mysqladmin -u root #{password_option} flush-privileges"
   action :nothing
 end
